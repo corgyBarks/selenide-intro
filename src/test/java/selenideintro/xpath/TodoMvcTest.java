b41e44c51fd4abcc59afb9f6fe276ac7cb7139e5
Aurika <ponylfn@gmail.com>
2022-11-03 23:37:47 +0200

    refactored

--- src/test/java/selenideintro/xpath/TodoMvcTest.java
+++ src/test/java/selenideintro/xpath/TodoMvcTest.java
@@ -16,11 +16,12 @@ public class TodoMvcTest {
         $(byXpath("//*[@id='new-todo']")).setValue("c").pressEnter();
         $$(byXpath("//*[@id='todo-list']/li")).shouldHave(exactTexts("a", "b", "c"));
 
-        $(byXpath("//*[@id='todo-list']//li[.//text()='b']//*[@class='toggle']")).click();
+        $(byXpath("//*[@id='todo-list']//li[.//text()='b']//*[contains(concat(' ', normalize-space(@class), ' '), ' toggle ')]"))
+                .click();
 
-        $$(byXpath("//*[@id='todo-list']/li[starts-with(@class,'completed')]"))
+        $$(byXpath("//*[@id='todo-list']//li[contains(concat(' ', normalize-space(@class), ' '), ' completed ')]"))
                 .shouldHave(exactTexts("b"));
-        $$(byXpath("//*[@id='todo-list']/li[not(starts-with(@class,'completed'))]"))
+        $$(byXpath("//*[@id='todo-list']//li[not(contains(concat(' ', normalize-space(@class), ' '), ' completed '))]"))
                 .shouldHave(exactTexts("a", "c"));
     }
 }
